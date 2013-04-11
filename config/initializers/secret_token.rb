89748f2f06cfc56118b480010923ae9f28483b9c
aditbiswas1 <aditbiswas1@gmail.com>
2013-04-12 00:13:48 +0530

    added the scaffolding for companies

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Esd::Application.config.secret_token = '69bed081cbc14ef58c597232928c5001bba9b02917fb352c959a89d24b8da5bef028cb9565385abdc17fb5b852d29a50ba16682dfe61b7b044a4f84533865c24'
---
+++ spec/views/companies/edit.html.erb_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe "companies/edit" do
+  before(:each) do
+    @company = assign(:company, stub_model(Company,
+      :name => "MyString",
+      :description => "MyText",
+      :email => "MyString"
+    ))
+  end
+
+  it "renders the edit company form" do
+    render
+
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "form", :action => companies_path(@company), :method => "post" do
+      assert_select "input#company_name", :name => "company[name]"
+      assert_select "textarea#company_description", :name => "company[description]"
+      assert_select "input#company_email", :name => "company[email]"
+    end
+  end
+end
